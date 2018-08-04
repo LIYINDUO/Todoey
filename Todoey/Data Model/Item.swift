4b53b79d04eeee40fb83e159fec27e1a370557ce
yinduo <liyinduo@gmail.com>
2018-08-04 22:51:41 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by LIYINDUO on 4/8/18.
+//  Copyright © 2018 LIYINDUO. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done :  Bool = false
+    
+}
